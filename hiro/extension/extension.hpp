79fd0185b622dbc2b220aa7cacec8f838024a361
Morten Delenk <mtg.morten@googlemail.com>
2016-07-03 13:59:53 +0200

    removed for a short time

--- hiro/extension/extension.hpp
+++
@@ -1,9 +0,0 @@
-namespace hiro {
-  #include "../resource/resource.hpp"
-  #include "fixed-layout.hpp"
-  #include "horizontal-layout.hpp"
-  #include "vertical-layout.hpp"
-  #include "shared.hpp"
-  #include "browser-dialog.hpp"
-  #include "message-dialog.hpp"
-}
--- nall/xorg/xorg.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <nall/xorg/guard.hpp>
-#include <sys/ipc.h>
-#include <sys/shm.h>
-#include <X11/Xlib.h>
-#include <X11/Xutil.h>
-#include <X11/Xatom.h>
-#include <nall/xorg/guard.hpp>
