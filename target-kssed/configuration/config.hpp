4246f766e96c461fa30b44d3ef22d83c8be2ede1
Morten Delenk <mtg.morten@googlemail.com>
2016-09-03 18:08:58 +0200

    cleaned repo

--- target-kssed/configuration/config.hpp
+++
@@ -1,8 +0,0 @@
-struct ConfigurationManager : Configuration::Document {
-  ConfigurationManager();
-  auto quit() -> void;
-  struct Video : Configuration::Node {
-    string driver;
-  } video;
-};
-extern ConfigurationManager* config;
